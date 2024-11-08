c8376ba473825d368468db4c1c5ea765df6f9063
asifahmadd53 <asifahmadd53@gmail.com>
2024-11-09 00:19:48 +0500

    final version

--- server/index.js
+++ server/index.js
@@ -19,6 +19,11 @@ app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 const port = process.env.PORT || 4000
 
 
+app.use('/',(req,res)=>{
+    res.send('Hii This is Bytvance')
+})
+
+
 app.use('/user',userRoutes)
 app.use('/blog',blogRoutes)
 app.use('/admin',adminRoute)
