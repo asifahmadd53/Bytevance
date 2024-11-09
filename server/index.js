397ea9708d4799ad920e4933384df794e595e70a
asifahmadd53 <asifahmadd53@gmail.com>
2024-11-09 17:21:49 +0500

    final version5

--- server/index.js
+++ server/index.js
@@ -14,16 +14,13 @@ app.use(cookieparser())
 
 app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 
-
-
 const port = process.env.PORT || 4000
-
-
 
 app.use('/user',userRoutes)
 app.use('/blog',blogRoutes)
 app.use('/admin',adminRoute)
 app.use('/auth',auth)
+
 
 app.listen(port,()=>{
     console.log(`Server is running on port http://localhost:${port}`)
